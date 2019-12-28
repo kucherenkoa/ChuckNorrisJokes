946041e147889eaa4ac1b2789d46aced025f5014
Andrei Kucherenko <kucherenkoa.io@gmail.com>
2019-12-28 07:50:42 +0300

    Initial Commit

---
+++ ChuckNorrisJokes/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ChuckNorrisJokes
+//
+//  Created by Andrei Kucherenko on 28.12.2019.
+//  Copyright © 2019 Andrei Kucherenko. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ChuckNorrisJokes/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ChuckNorrisJokes
+//
+//  Created by Andrei Kucherenko on 28.12.2019.
+//  Copyright © 2019 Andrei Kucherenko. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
