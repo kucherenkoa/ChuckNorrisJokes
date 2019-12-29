198d790fb629bee61cdf34502d1a6abe7010d2a6
Andrei Kucherenko <kucherenkoa.io@gmail.com>
2019-12-29 08:52:57 +0300

    Add  WKWebview with progress bar. Show  Chack Norris jokes API  page.  Implement launch screen.

--- ChuckNorrisJokes/APIViewController.swift
+++ ChuckNorrisJokes/APIViewController.swift
@@ -7,14 +7,77 @@
 //
 
 import UIKit
+import WebKit
 
 class APIViewController: UIViewController {
-
+    var webView: WKWebView!
+    let progressView = UIProgressView(progressViewStyle: .default)
+    private var estimatedProgressObserver: NSKeyValueObservation?
+    
+    override func loadView() {
+        webView = WKWebView()
+        webView.navigationDelegate = self
+        view = webView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupProgressView()
+        setupEstimatedProgressObserver()
+        
+        if let initialUrl = URL(string: "http://www.icndb.com/api/") {
+            setupWebview(url: initialUrl)
+        }
+        
     }
-
-
+    
+    // MARK: - Setup ProgressView
+    private func setupProgressView() {
+        guard let navigationBar = navigationController?.navigationBar else { return }
+        
+        progressView.translatesAutoresizingMaskIntoConstraints = false
+        navigationBar.addSubview(progressView)
+        progressView.isHidden = true
+        
+        NSLayoutConstraint.activate([
+            progressView.leadingAnchor.constraint(equalTo: navigationBar.leadingAnchor),
+            progressView.trailingAnchor.constraint(equalTo: navigationBar.trailingAnchor),
+            
+            progressView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor),
+            progressView.heightAnchor.constraint(equalToConstant: 2.0)
+        ])
+    }
+    
+    private func setupEstimatedProgressObserver() {
+        estimatedProgressObserver = webView.observe(\.estimatedProgress, options: [.new]) { [weak self] webView, _ in
+            self?.progressView.isHidden = webView.estimatedProgress < 1 ? false : true
+            self?.progressView.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    // MARK: - Setup Webview
+    private func setupWebview(url: URL) {
+        let request = URLRequest(url: url)
+        
+        webView.navigationDelegate = self
+        webView.load(request)
+    }
+    
 }
 
+// MARK: - WKNavigationDelegate
+extension APIViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        debugPrint("didCommit")
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        debugPrint("didFinish")
+    }
+    
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        debugPrint("didFail")
+    }
+    
+}
